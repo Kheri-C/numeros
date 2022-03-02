ede565effa6206e3354a9681be30e1ec5a200418
Kheri-C <88911105+Kheri-C@users.noreply.github.com>
2022-03-02 13:30:53 -0600

    Innitial Commit

--- app/src/main/java/edu/itesm/numeros/MainActivity.kt
+++ app/src/main/java/edu/itesm/numeros/MainActivity.kt
@@ -2,10 +2,69 @@ package edu.itesm.numeros
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import edu.itesm.numeros.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding : ActivityMainBinding
+    val numeros = mutableListOf<Int>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        initUI()
+    }
+
+    private fun initUI(){
+        binding.agregar.setOnClickListener {
+            interfaceAddNumber()
+        }
+        binding.promedio.setOnClickListener {
+            interfaceAverage()
+        }
+        binding.mayor.setOnClickListener {
+            interfaceHighestNumber()
+        }
+        binding.pares.setOnClickListener {
+            interfaceEvenNumbers()
+        }
+    }
+
+    private fun interfaceAddNumber(){
+        val newNumberValue = binding.editTextNumberSigned.text.toString().toIntOrNull()
+        //val newNumberValue = newNumber.toIntOrNull()
+        if(newNumberValue == null){
+            Toast.makeText(this,"Número inválido, prueba nuevamente",Toast.LENGTH_SHORT).show()
+            return
+        }
+        numeros.add(newNumberValue)
+        Toast.makeText(this,"El número ${newNumberValue} ha sido agregado",Toast.LENGTH_SHORT).show()
+    }
+
+    private fun interfaceHighestNumber(){
+        var highestNumber = numeros.maxOrNull()
+        if(numeros.isEmpty()){
+            Toast.makeText(this,"No hay ningún número todavía",Toast.LENGTH_SHORT).show()
+            return
+        }
+        binding.resultados.text = highestNumber.toString()
+    }
+
+    private fun interfaceEvenNumbers(){
+        val numerosPares = numeros.filter { x -> x % 2 == 0 }.average()
+        if(numerosPares.isNaN()){
+            Toast.makeText(this,"No hay ningún número par",Toast.LENGTH_SHORT).show()
+            return
+        }
+        binding.resultados.text = numerosPares.toString()
+    }
+
+    private fun interfaceAverage(){
+        val numeroPromedio = numeros.average()
+        if(numeroPromedio.isNaN()) {
+            Toast.makeText(this,"No hay ningún número todavía",Toast.LENGTH_SHORT).show()
+            return
+        }
+        binding.resultados.text = numeroPromedio.toString()
     }
 }
